1f7fb6423399024a316e1139ffe15e3a232faf10
emre <emreguney@gmail.com>
2009-08-28 02:52:05 +0200

    Reorganizing dir structure for goh & rhodes network, creating node and score files and random sampling of graphs.

--- main.py
+++ main.py
@@ -12,34 +12,96 @@ import analyze_results
 import os, os.path
 
 # Scoring related parameters 
-DEFAULT_NON_SEED_SCORE = 0
+#PPI = "biana" 
+#PPI = "goh" 
+PPI = "rhodes"
+DEFAULT_NON_SEED_SCORE = 0.01
+ALLOWED_MAX_DEGREE = 100000 #175 #90
+N_SAMPLE_GRAPH = 10 #100
 
 # Project directory structure
 data_dir = ".." + os.sep + "data" + os.sep
-input_dir = data_dir + os.sep + "input" + os.sep
-output_dir = data_dir + os.sep + "output" + os.sep
-sampling_dir = data_dir + "sampled_grahps" + os.sep
+#input_dir = data_dir + "input" + os.sep
+#output_dir = data_dir + "output" + os.sep
+#sampling_dir = input_dir + "sampled_graphs" + os.sep
 
 # BIANA node & network files
-node_file_prefix = data_dir + "human_interactome_biana" + os.sep + "human_nodes"
-network_file_prefix = data_dir + "human_interactome_biana" + os.sep + "human_network"
-network_file_filtered_by_method = network_file_prefix + "_no_tap.sif"
-network_file_filtered_by_degree = network_file_filtered_by_method[:-4] + "no_hub.sif"
+biana_node_file_prefix = data_dir + "human_interactome_biana" + os.sep + "human_nodes"
+biana_network_file_prefix = data_dir + "human_interactome_biana" + os.sep + "human_network"
+biana_network_file_filtered_by_method = biana_network_file_prefix + "_no_tap.sif"
+biana_network_file_filtered_by_degree = biana_network_file_filtered_by_method[:-4] + "_degree_filtered.sif"
+
+# Edge attribute files
+biana_network_file_method_attribute = biana_network_file_prefix + "_method_id.eda"
+biana_network_file_source_attribute = biana_network_file_prefix + "_source.eda"
+biana_network_file_pubmed_attribute = biana_network_file_prefix + "_pubmed.eda"
+
+# PPIs from existing studies
+goh_network_file = data_dir + "goh07_human_ppi" + os.sep + "ppi.sif"
+rhodes_network_file = data_dir + "rhodes05_human_probabilistic_ppi" + os.sep + "ppi.sif"
+goh_network_file_filtered_by_degree = goh_network_file[:-4] + "_degree_filtered.sif"
+rhodes_network_file_filtered_by_degree = rhodes_network_file[:-4] + "_degree_filtered.sif"
+
+# Gene info file 
+gene_info_file = data_dir + "gene_info" + os.sep + "genes.tsv"
 
 # Disease association files
 aneurysm_scores_file = data_dir + "aneurist" + os.sep + "aneurysm_associated_genes.txt"
 aneurysm_scores_all_equal_file = data_dir + "aneurist" + os.sep + "aneurysm_associated_genes_all_equal.txt"
-association_scores_file = aneurysm_scores_all_equal_file
 
-# Node & edge score files
-node_scores_file = input_dir + "human_node_scores.sif"
-edge_scores_file = input_dir + "human_filtered.sif"
+
+# Network specific
+
+# biana ppi
+if PPI == "biana":
+    node_description_file = biana_node_file_prefix + ".tsv"
+    network_file = biana_network_file_filtered_by_method
+    network_file_filtered = biana_network_file_filtered_by_degree 
+    input_dir = data_dir + "input_biana" + os.sep
+    output_dir = data_dir + "output_biana" + os.sep
+    network_file_identifier_type = "user entity id"
+# goh ppi
+elif PPI == "goh":
+    node_description_file = gene_info_file 
+    network_file = goh_network_file
+    network_file_filtered = goh_network_file_filtered_by_degree 
+    input_dir = data_dir + "input_goh" + os.sep
+    output_dir = data_dir + "output_goh" + os.sep
+    network_file_identifier_type = "geneid"
+# rhodes ppi
+elif PPI == "rhodes":
+    node_description_file = gene_info_file 
+    network_file = rhodes_network_file
+    network_file_filtered = rhodes_network_file_filtered_by_degree 
+    input_dir = data_dir + "input_rhodes" + os.sep
+    output_dir = data_dir + "output_rhodes" + os.sep
+    network_file_identifier_type = "geneid"
+else:
+    raise ValueError("Unrecognized ppi!")
+
+# common to all ppi
+association_scores_file = aneurysm_scores_all_equal_file
+association_scores_file_identifier_type = "genesymbol"
+
+node_scores_file = input_dir + "node_scores.sif"
+edge_scores_file = input_dir + "edge_scores.sif"
+sampling_dir = input_dir + "sampled_graphs" + os.sep
+
 sampled_file_prefix = sampling_dir + "sampled_graph"
 
-# Filtered files
-network_file_method_attribute = network_file_prefix + "_method_id.eda"
-network_file_source_attribute = network_file_prefix + "_source.eda"
-network_file_pubmed_attribute = network_file_prefix + "_pubmed.eda"
+# Node & edge score files
+#node_scores_file = input_dir + "human_node_scores.sif"
+#edge_scores_file = input_dir + "human_edge_scores.sif"
+#goh_node_scores_file = input_dir + "goh_node_scores.sif"
+#goh_edge_scores_file = input_dir + "goh_edge_scores.sif"
+#rhodes_node_scores_file = input_dir + "rhodes_node_scores.sif"
+#rhodes_edge_scores_file = input_dir + "rhodes_edge_scores.sif"
+
+# Sampling related
+#sampled_file_prefix = sampling_dir + "sampled_graph"
+#goh_sampled_file_prefix = sampling_dir[:-1] + "_goh" + os.sep + "sampled_graph"
+#rhodes_sampled_file_prefix = sampling_dir[:-1] + "_rhodes" + os.sep + "sampled_graph"
+
 
 # Old scoring files
 node_file_netzcore = "../data/input/node_scores.txt"
@@ -63,7 +125,7 @@ netzcore_results_1 = output_dir + "netzcore_onlyEdgeRelevance_noNodeInitialScore
 netzcore_results_3 = output_dir + "netzcore_onlyEdgeRelevance_noNodeInitialScoreAccumulation_i3.txt"
 
 def main():
-    prepare(filter_hubs = False, sample_graphs = False)
+    prepare()
     #analyze()
     #score()
     #test_timing_score_one_fold()
@@ -88,45 +150,60 @@ def score():
     score_network.run_and_assess_performance_of_folds(10, edge_file_netzcore, edge_file_netzcore_relevance_prefix, node_file_netzcore, node_file_netzcore_prefix, result_file_prefix)
     return
 
-def prepare(filter_hubs = False, sample_graphs = False):
-    """
-	Creates necessary files for scoring
-    """
 
+def create_biana_network():
     # Create PPI network using BIANA
-    if not (os.path.exists(node_file_prefix+".tsv") and os.path.exists(network_file_prefix+".sif")):
-	print "Creating BIANA Human interactome"
-	prepare_data.create_human_interactome_using_biana(node_file_prefix = node_file_prefix, network_files_prefix = network_file_prefix, network_type="functional", load_from_saved_session=True) #"experimental") 
+    if not (os.path.exists(biana_node_file_prefix+".tsv") and os.path.exists(biana_network_file_prefix+".sif")):
+	print "Creating BIANA Human interactome", biana_node_file_prefix+".tsv", biana_network_file_prefix+".sif"
+	prepare_data.create_human_interactome_using_biana(node_file_prefix = biana_node_file_prefix, network_files_prefix = biana_network_file_prefix, network_type="functional", load_from_saved_session=False) #"experimental")  
 
     # Filter by detection method (non-tap interactions)
-    if not os.path.exists(network_file_filtered_by_method): 
-	print "Creating method filtered files"
-	prepare_data.create_method_filtered_network_files(network_file_method_attribute = network_file_prefix + "_method_id.eda", network_file_method_filtered_prefix = network_file_prefix)
+    if not os.path.exists(biana_network_file_filtered_by_method): 
+	print "Creating method filtered files",  biana_network_file_prefix + ".sif", "->", biana_network_file_filtered_by_method
+	prepare_data.create_method_filtered_network_files(network_file_method_attribute = biana_network_file_prefix + "_method_id.eda", network_file_method_filtered_prefix = biana_network_file_prefix)
+    return
 
-	# Filter network by degree and get largest connected component
-	if filter_hubs:
-	    print "Filtering by degree"
-	    #prepare_data.create_degree_filtered_network_file(network_file_filtered_by_method, network_file_filtered_by_degree, 90)
-	    prepare_data.create_degree_filtered_network_file(network_file_filtered_by_method, network_file_filtered_by_degree, 175)
-	    network_file_filtered  =  network_file_filtered_by_degree
-	else: 
-	    print "No degree filtering, using method filtered file!"
-	    network_file_filtered = network_file_filtered_by_method
+def prepare_scoring_files_from_network_and_association_files(network_file, network_file_filtered, network_file_identifier_type, node_description_file, association_scores_file, association_scores_file_identifier_type, node_scores_file, edge_scores_file, sampled_file_prefix):
+    # Filter network by degree and get largest connected component
+    if not os.path.exists(network_file_filtered): 
+	print "Filtering by degree", network_file, "->", network_file_filtered
+	prepare_data.create_degree_filtered_network_file(network_file, network_file_filtered, ALLOWED_MAX_DEGREE)
 
     # Create node scores file and check how many of the seed genes we cover in the network
     if not os.path.exists(node_scores_file): 
-	print "Creating node scores file"
-	prepare_data.create_node_scores_file(network_file = network_file_filtered, network_file_identifier_type = "user entity id", node_biana_file = node_file_prefix + ".tsv", association_scores_file = association_scores_file, association_scores_file_identifier_type = "genesymbol", node_scores_file = node_scores_file, ignored_seed_nodes = None, default_non_seed_score = DEFAULT_NON_SEED_SCORE)
-
+	print "Creating node scores file", node_scores_file
+	prepare_data.create_node_scores_file(network_file = network_file_filtered, network_file_identifier_type = network_file_identifier_type, node_description_file = node_description_file, association_scores_file = association_scores_file, association_scores_file_identifier_type = association_scores_file_identifier_type, node_scores_file = node_scores_file, ignored_seed_nodes = None, default_non_seed_score = DEFAULT_NON_SEED_SCORE)
+    if not os.path.exists(edge_scores_file): 
+	print "Creating edge scores file", edge_scores_file
+	prepare_data.create_edge_scores_file(network_file = network_file_filtered, edge_scores_file = edge_scores_file)
+    if not os.path.exists(sampled_file_prefix + ".sif.1"): 
+	print "Creating sampled networks"
+	prepare_data.sample_network_preserving_topology(edge_scores_file, N_SAMPLE_GRAPH, sampled_file_prefix + ".sif.")
     return
 
+
+def prepare_scoring_files():
+    prepare_scoring_files_from_network_and_association_files(network_file, network_file_filtered, network_file_identifier_type, node_description_file, association_scores_file, association_scores_file_identifier_type, node_scores_file, edge_scores_file, sampled_file_prefix)
+    return
+
+
+def prepare():
+    """
+	Creates necessary files for scoring
+    """
+    if not os.path.exists(input_dir): 
+	os.mkdir(input_dir)
     if not os.path.exists(sampling_dir): 
 	os.mkdir(sampling_dir)
-	
-    if sample_graphs:
-	print "Creating sampled networks"
-	prepare_data.sample_network_preserving_topology(edge_file_netzcore_relevance[:-4] + ".sif", 100, sampled_file_prefix + ".sif.")
-    #prepare_data.sample_network_preserving_topology("../data/toy_data/test_interactions_small.sif", 4, "../data/sampled_graphs/sampled_graph.sif.")
+    if not os.path.exists(output_dir): 
+	os.mkdir(output_dir)
+
+    if PPI == "biana":
+	create_biana_network()
+
+    prepare_scoring_files()
+
+    return
 
     # Get scores of each node
     node_to_score, seeds_all = prepare_data.map_scores_to_biana_nodes(node_file, node_file_score)
@@ -144,7 +221,7 @@ def prepare(filter_hubs = False, sample_graphs = False):
     prepare_data.generate_cross_validation_node_files(g = g, node_to_score = node_to_score, seeds = seeds, node_file_netzcore_prefix = node_file_netzcore_prefix, edge_file_netzcore_relevance_prefix =  edge_file_netzcore_relevance_prefix, arff_file_prefix = arff_file_prefix)
 
     # Assign edge scores
-    prepare_data.assign_edge_reliability_scores(g = g, network_file_method_attribute = network_file_method_attribute, network_file_source_attribute = network_file_source_attribute, network_file_pubmed_attribute = network_file_pubmed_attribute, out_file = edge_file_netzcore)
+    prepare_data.assign_edge_reliability_scores(g = g, network_file_method_attribute = biana_network_file_method_attribute, network_file_source_attribute = biana_network_file_source_attribute, network_file_pubmed_attribute = biana_network_file_pubmed_attribute, out_file = edge_file_netzcore)
 
     #create_arff_file(node_score_file, network_file_filtered, arff_file)
 
--- prepare_data.py
+++ prepare_data.py
@@ -7,14 +7,7 @@
 from biana.utilities import biana_output_converter
 from biana.utilities import graph_utilities as network_utilities
 
-def sample_network_preserving_topology(network_sif_file, n_sample, output_prefix):
-    g = network_utilities.create_network_from_sif_file(network_file_in_sif = network_sif_file, weighted = True, delim = " ")
-    for i in xrange(1,n_sample+1):
-	g_sampled = network_utilities.randomize_graph(graph=g, randomization_type="preserve_topology_and_node_degree")
-	network_utilities.output_network_in_sif(g_sampled, output_prefix+"%s"%i)
-    return
-
-def generate_cross_validation_node_files(g, node_to_score, seeds, node_file_netzcore_prefix, edge_file_netzcore_relevance_prefix, arff_file_prefix, edge_file_netzcore):
+def old_generate_cross_validation_node_files(g, node_to_score, seeds, node_file_netzcore_prefix, edge_file_netzcore_relevance_prefix, arff_file_prefix, edge_file_netzcore):
     # seeds = [ v for v, s in node_to_score.iteritems() if s != 0 ] # was assuming that non-seeds should have zero score
     for k, training, test in k_fold_cross_validation(seeds, 10, randomize = True, useSeed = True):
 	assign_node_scores(g = g, node_to_score = node_to_score, out_file = node_file_netzcore_prefix+"_%i.txt"%k, as_edge_relevance_score = False, seeds_ignored=test)
@@ -23,7 +16,7 @@ def generate_cross_validation_node_files(g, node_to_score, seeds, node_file_netz
 	network_utilities.create_arff_file_with_network_metrics(g, node_to_score, training, arff_file_prefix+"_%i.arff"%k)
     return
 
-def create_edge_file_from_weight_and_score_files(edge_file_weights, edge_file_scores, out_file):
+def old_create_edge_file_from_weight_and_score_files(edge_file_weights, edge_file_scores, out_file):
     setNode, setEdge, dictNode, dictEdgeWeight = network_utilities.get_nodes_and_edges_from_sif_file(file_name = edge_file_weights[:-3]+"sif", store_edge_type = True)
     setNode, setEdge, dictNode, dictEdge = network_utilities.get_nodes_and_edges_from_sif_file(file_name = edge_file_scores[:-3]+"sif", store_edge_type = True)
     f = open(out_file, "w")
@@ -63,7 +56,7 @@ def k_fold_cross_validation(X, K, randomize = False, useSeed = False):
 	    yield k, training, validation
 
 
-def extract_test_data_from_arff_files(arff_file_all, arff_file_filtered, arff_file_training, arff_file_test):
+def old_extract_test_data_from_arff_files(arff_file_all, arff_file_filtered, arff_file_training, arff_file_test):
     f_all = open(arff_file_all)
     f_training = open(arff_file_training)
     f_filtered = open(arff_file_filtered)
@@ -88,13 +81,13 @@ def extract_test_data_from_arff_files(arff_file_all, arff_file_filtered, arff_fi
     return
 
 
-def generate_cross_validation_test_test_arff_files(k, arff_file_all, arff_file_filtered, arff_file_training_prefix):
+def old_generate_cross_validation_test_test_arff_files(k, arff_file_all, arff_file_filtered, arff_file_training_prefix):
     for i in xrange(k):
 	extract_test_data_from_arff_files(arff_file_all, arff_file_filtered, arff_file_training_prefix+"_%i.arff"%i, arff_file_training_prefix+"_test_%i.arff"%i)
     return
 
 
-def assign_edge_reliability_scores(g, network_file_method_attribute, network_file_source_attribute, network_file_pubmed_attribute, out_file):
+def old_assign_edge_reliability_scores(g, network_file_method_attribute, network_file_source_attribute, network_file_pubmed_attribute, out_file):
     # linear combination of 
     # pubmed/5
     # db/3
@@ -127,14 +120,30 @@ def assign_edge_reliability_scores(g, network_file_method_attribute, network_fil
     f.close()
     return
 
+def sample_network_preserving_topology(network_sif_file, n_sample, output_prefix):
+    g = network_utilities.create_network_from_sif_file(network_file_in_sif = network_sif_file, use_edge_data = True, delim = " ")
+    for i in xrange(1,n_sample+1):
+	g_sampled = network_utilities.randomize_graph(graph=g, randomization_type="preserve_topology_and_node_degree")
+	network_utilities.output_network_in_sif(g_sampled, output_prefix+"%s"%i)
+    return
 
-def create_edge_scores_file_from_node_scores(network_file, node_scores_file, ignored_seed_nodes = None, default_non_seed_score = 0):
+
+def create_edge_scores_file(network_file, edge_scores_file):
+    g = network_utilities.create_network_from_sif_file(network_file)
+    f = open(edge_scores_file, 'w')
+    for u,v in g.edges_iter():
+	f.write("%s %d %s\n" % (u, 1, v))
+    f.close()
+    return
+
+
+def create_edge_scores_file_from_node_scores(network_file, node_scores_file, edge_scores_file, ignored_seed_nodes = None, default_non_seed_score = 0):
     """
 	Creates edge score file from node association scores, intended comparing netshort with other algorithms without using other edge reliability/relevance score
     """
     g = network_utilities.create_network_from_sif_file(network_file)
     setNode, setDummy, dictNode, dictDummy = network_utilities.get_nodes_and_edges_from_sif_file(file_name = node_scores_file, store_edge_type = False)
-    f = open(node_scores_file, 'w')
+    f = open(edge_scores_file, 'w')
     for u,v in g.edges_iter():
 	if ignored_seed_nodes is not None and u in ignored_seed_nodes:
 	    score_u = default_non_seed_score
@@ -144,49 +153,52 @@ def create_edge_scores_file_from_node_scores(network_file, node_scores_file, ign
 	    score_v = default_non_seed_score
 	else:
 	    score_v = dictNode[v]
-	f.write("%s\t%s\t%f\n" % (u, v, (score_u + score_v) / 2))
+	f.write("%s %f %s\n" % (u, (score_u + score_v) / 2, v))
     f.close()
     return
 
 
-def create_node_scores_file(network_file, network_file_identifier_type, node_biana_file, association_scores_file, association_scores_file_identifier_type, node_scores_file, ignored_seed_nodes = None, default_non_seed_score = 0):
+def create_node_scores_file(network_file, network_file_identifier_type, node_description_file, association_scores_file, association_scores_file_identifier_type, node_scores_file, ignored_seed_nodes = None, default_non_seed_score = 0):
     """
 	Creates node association/relevance score file for the nodes in the network using given association_scores_file, correspondance identifiers
 	ignored_seed_nodes: nodes in this set are ignored
     """
     g = network_utilities.create_network_from_sif_file(network_file)
+    nodes = g.nodes()
     setNode, setDummy, dictNode, dictDummy = network_utilities.get_nodes_and_edges_from_sif_file(file_name = association_scores_file, store_edge_type = False)
-    node_to_genes, gene_to_nodes = biana_output_converter.get_attribute_to_attribute_mapping(node_biana_file, network_file_identifier_type, association_score_file_identifier_type)
+    node_to_genes, gene_to_nodes = biana_output_converter.get_attribute_to_attribute_mapping(node_description_file, network_file_identifier_type, association_scores_file_identifier_type, keys_to_include=set(nodes))
 
     f = open(node_scores_file, 'w')
     covered_genes = set()
-    for v in g.nodes_iter():
+    seeds = set()
+    for v in nodes: #g.nodes_iter():
 	gene_with_score = setNode & node_to_genes[v]
 	covered_genes |= gene_with_score
 
 	if len(gene_with_score) > 0:
+	    seeds.add(v)
 	    if len(gene_with_score) > 1:
 		print "More than one gene:", gene_with_score, "for", v
 	    i=0
+	    score = 0
 	    for gene in covered_genes:
 		i+=1
 		score += float(dictNode[gene])
 	    score /= i
+	    if score <= 0:
+		print "non-positive seed score", v, score, "genes:", node_to_genes[v]
 	else:
 	    score = default_non_seed_score
-
-	if score <= 0:
-	    print "non-positive seed score", gene, score/i
 
 	if ignored_seed_nodes is not None and v in ignored_seed_nodes:
 	    score_v = default_non_seed_score
 	else:
 	    score_v = score
-	f.write("%s\t%f\n" % (v, score_v))
+	f.write("%s %f\n" % (v, score_v))
 
     f.close()
     print "Covered genes (seed genes):", len(covered_genes), "among", len(setNode)
-    print "Covered gene products (seed nodes):", len(seeds), "among", len(g.number_of_nodes())
+    print "Covered gene products (seed nodes):", len(seeds), "among", g.number_of_nodes()
     return 
 
 
@@ -195,12 +207,12 @@ def create_degree_filtered_network_file(network_file, network_file_filtered, deg
 	Creates a network file removing nodes that has connections more than given degree
     """
     # Load network
-    g = network_utilities.create_network_from_sif_file(network_file)
+    g = network_utilities.create_network_from_sif_file(network_file, use_edge_data = True)
     network_utilities.analyze_network(g)
-    g = network_utilities.filter_network(g, degree) 
+    g = network_utilities.filter_network(g = g, degree_threshold = degree, largest_connected_component=True) 
     # Get degrees of highly connected nodes
     network_utilities.analyze_network(g)
-    output_network_in_sif(g, network_file_filtered)
+    network_utilities.output_network_in_sif(g, network_file_filtered)
     return
 
 
@@ -238,7 +250,7 @@ def create_human_interactome_using_biana(node_file_prefix="human_nodes", network
     relation_attributes=["pubmed", "method_id"]
     if network_type == "functional":
 	relation_type_list = ["functional_association"] 
-	relation_attributes.append("stringscore")
+	relation_attributes.extend(["stringscore", "stringscore_neighborhood", "stringscore_fusion", "stringscore_cooccurence", "stringscore_experimental", "stringscore_db", "stringscore_textmining"])
 	# Uncommet below for only relations from STRING with score > 700
 	#relation_attribute_restriction_list=[("stringscore",">700")]
     elif network_type == "experimental":
@@ -268,7 +280,7 @@ def create_human_interactome_using_biana(node_file_prefix="human_nodes", network
 	save_session("biana_session", network_files_prefix + "_session.dat")
 
     # Output set details - all nodes
-    #objSession.output_user_entity_set_details(user_entity_set_id = "User_Entity_Set_1", out_method = open(node_file_prefix + ".tsv", "w").write, attributes=node_attributes, include_level_info=False, include_degree_info=True, level=None, only_selected=False, output_format="tabulated", include_tags_info = True, include_tags_linkage_degree_info=[], substitute_node_attribute_if_not_exists=False, output_1_value_per_attribute=False, include_command_in_rows=False)
+    objSession.output_user_entity_set_details(user_entity_set_id = "User_Entity_Set_1", out_method = open(node_file_prefix + ".tsv", "w").write, attributes=node_attributes, include_level_info=False, include_degree_info=True, level=None, only_selected=False, output_format="tabulated", include_tags_info = True, include_tags_linkage_degree_info=[], substitute_node_attribute_if_not_exists=False, output_1_value_per_attribute=False, include_command_in_rows=False)
 	
     # Export all the information of this set and its network to a file in a tab separated format
     objSession.output_user_entity_set_network_in_sif_format(user_entity_set_id = "User_Entity_Set_1", output_path = "./", output_prefix = network_files_prefix, node_attributes = [], participant_attributes = [], relation_attributes = relation_attributes, output_1_value_per_attribute = False, include_tags = False) 
